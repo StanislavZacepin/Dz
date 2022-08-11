d56ad6c4fea8a3f20eeb328555e37e650121c107
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-11 18:47:46 +0300

    Проверка  деагональ снизу

--- Program.java
+++ Program.java
@@ -32,6 +32,11 @@ public static  void print(int[][] board){
             for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
                 if (board[i][j] == 1)
                     return false;
+            for (i = row, j = col; j >= 0 && i < N; i++, j--)
+                if (board[i][j] == 1)
+                    return false;
+
+            return true;
         }
 
     }
