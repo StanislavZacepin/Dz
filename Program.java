c79cbfccff7a26177bdbe8bb45436756e81cb455
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-11 18:36:37 +0300

    метод печати

--- Program.java
+++ Program.java
@@ -1,52 +1,81 @@
 import  java.util.ArrayList;
 import  java.util.List;
 public class Program {
-    List<List<Integer>> ans = new ArrayList<>();
+
+    public static int count = 0;
+    public static boolean Flag = false;
+
     public static void main(String[] args) {
 
-        /*int[] nums = new int[] {5,4,3,2,1};
-        mergeSort(nums, 0, nums.length -1 );
+        int[][] board = new int[8][8];
+        print(board);
+
 
     }
-    public static void mergeSort(int [] nums, int start, int end) {
-        if (start >= end) return;
-
-        int mid = (start+end)/2;
-        mergeSort(nums, start,mid);
-        mergeSort(nums, mid+1, end);
-        merge(nums, start, mid, end);
+public static  void print(int[][] board){
+    int n = board.length;
+    System.out.println("------------------------------");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            System.out.print(board[i][j] + "|");
         }
-
-        public static void merge(int[] nums, int start, int mid, int end){
-        int[] temp = new int[end - start + 1];
-        int right = mid +1;
-        int k = 0;
-        while (start <= mid && right <= end) {
-            if(nums[start] < nums[right]){
-                temp[k++] = nums[start++];
-            }else {
-                temp[k++] = nums[right++];
-            }
-        }
-        while (start <= mid) temp[k++] = nums[start++];
-        while (right <= end) temp[k++] = nums[right++];
-
-        System.arraycopy(temp, 0, nums, start, end + 1 - start);
-        }*/
+        System.out.println();
     }
-    public List<List<Integer>> combine(int n, int k) {
-        genComb(n, k, 0, new ArrayList<>());
-        return ans;
-    }
-    private void genComb(int n, int k, int prevEl, List<Integer> curComb){
-        if(curComb.size() == k){
-            ans.add(new ArrayList<>(curComb));
-        }
-
-        for (int i = prevEl + 1; i <= n; i++){
-            curComb.add(i);
-            genComb(n, k, i, curComb);
-            curComb.remove(curComb.size() - 1);
-        }
-    }
+    System.out.println("------------------------------");
 }
+
+}
+
+
+
+
+//public  static
+//        List<List<Integer>> ans = new ArrayList<>();
+//    public static void main(String[] args) {
+//
+//        /*int[] nums = new int[] {5,4,3,2,1};
+//        mergeSort(nums, 0, nums.length -1 );
+//
+//    }
+//    public static void mergeSort(int [] nums, int start, int end) {
+//        if (start >= end) return;
+//
+//        int mid = (start+end)/2;
+//        mergeSort(nums, start,mid);
+//        mergeSort(nums, mid+1, end);
+//        merge(nums, start, mid, end);
+//        }
+//
+//        public static void merge(int[] nums, int start, int mid, int end){
+//        int[] temp = new int[end - start + 1];
+//        int right = mid +1;
+//        int k = 0;
+//        while (start <= mid && right <= end) {
+//            if(nums[start] < nums[right]){
+//                temp[k++] = nums[start++];
+//            }else {
+//                temp[k++] = nums[right++];
+//            }
+//        }
+//        while (start <= mid) temp[k++] = nums[start++];
+//        while (right <= end) temp[k++] = nums[right++];
+//
+//        System.arraycopy(temp, 0, nums, start, end + 1 - start);
+//        }*/
+//    }
+//    public List<List<Integer>> combine(int n, int k) {
+//        genComb(n, k, 0, new ArrayList<>());
+//        return ans;
+//    }
+//    private void genComb(int n, int k, int prevEl, List<Integer> curComb){
+//        if(curComb.size() == k){
+//            ans.add(new ArrayList<>(curComb));
+//        }
+//
+//        for (int i = prevEl + 1; i <= n; i++){
+//            curComb.add(i);
+//            genComb(n, k, i, curComb);
+//            curComb.remove(curComb.size() - 1);
+//        }
+//    }
+//}
